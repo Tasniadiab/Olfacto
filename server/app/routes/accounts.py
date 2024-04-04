504fd8ff189e745d8eb3bd66858c8d7051f5ba86
Tasnia Bhuiyan <tasniadiab@gmail.com>
2024-04-04 02:44:51 -0400

    user registration update

--- server/app/routes/accounts.py
+++ server/app/routes/accounts.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, jsonify
 from flask_bcrypt import Bcrypt
 from models.accounts import User
 from app import create_app,db
@@ -7,7 +7,7 @@ from app import create_app,db
 app = create_app()
 
 bcrypt = Bcrypt(app)
-@app.route("/register")
+@app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"]
     username = request.json["username"]
@@ -18,8 +18,11 @@ def register_user():
     existing_email = User.query.filter_by(email=email).first() is not None
     existing_username = User.query.filter_by(username=username).first() is not None
 
-    if existing_email or existing_username:
-        abort(409)
+    if existing_email:
+        return jsonify({"error": "email already exists"})
+    
+    if existing_username:
+        return jsonify({"error": "username already exists"})
 
     hashed_password = bcrypt.generate_password_hash(password)
     new_user = User(email=email, username=username, firstname=firstname, lastname=lastname, password=hashed_password)
